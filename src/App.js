497ba4e8fd08b7a9bc753731eaf92745dae90eb5
Lam Tan Nghi <tannghil2307@gmail.com>
2023-11-29 20:16:11 +0700

    Sidabar design complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
 
       {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
